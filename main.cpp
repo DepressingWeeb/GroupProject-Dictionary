7dd5984ac44355b1c64f016e4da12d33f429daa3
Buu Pham <pphambuu@gmail.com>
2023-07-23 20:03:26 +0700

    .

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "DictionaryCLI.h"
-#include "DictionaryGUI.h" //Must download SDL and has SDL linked
+//#include "DictionaryGUI.h" //Must download SDL and has SDL linked
 
 int main(int, char**) {
 	SetConsoleOutputCP(CP_UTF8);
 	setvbuf(stdout, nullptr, _IONBF, 0);
-	//runCLI();
-	runGUI(); //Must download SDL and has SDL linked
+	runCLI();
+	//runGUI(); //Must download SDL and has SDL linked
 	return 0;
 }
