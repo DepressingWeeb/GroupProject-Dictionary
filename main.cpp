53cee887fdee0d92ff5393b2ad5ed14385d990eb
Buu Pham <pphambuu@gmail.com>
2023-07-23 12:03:27 +0700

    update input console

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 //#include "DictionaryGUI.h" //Must download SDL and has SDL linked
 
 int main(int, char**) {
+	SetConsoleOutputCP(CP_UTF8);
+	setvbuf(stdout, nullptr, _IONBF, 0);
 	runCLI();
 	//runGUI(); //Must download SDL and has SDL linked
 	return 0;
